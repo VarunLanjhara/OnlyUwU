ff0ff4b3973f31443c923ccdfb1007464e98873b
Painman <painman@gmail.com>
2022-03-20 00:59:33 +0530

    Update post stuff done

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -9,6 +9,7 @@ import {
   Button,
   useDisclosure,
   useToast,
+  Input,
 } from "@chakra-ui/react";
 import {
   BiDotsVerticalRounded,
@@ -30,7 +31,30 @@ import {
   AlertDialogContent,
   AlertDialogOverlay,
 } from "@chakra-ui/react";
-import { doc, deleteDoc, getFirestore } from "firebase/firestore";
+import {
+  doc,
+  deleteDoc,
+  getFirestore,
+  setDoc,
+  collection,
+  serverTimestamp,
+} from "firebase/firestore";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   posts?: {
@@ -47,6 +71,11 @@ type Props = {
 const Post = (props: Props) => {
   const auth = getAuth(app);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isEditOpen,
+    onOpen: onEditOpen,
+    onClose: onEditClose,
+  } = useDisclosure();
   const cancelRef = React.useRef();
   const db = getFirestore(app);
   const toast = useToast();
@@ -76,6 +105,110 @@ const Post = (props: Props) => {
           isClosable: true,
         });
       });
+  };
+  const [caption, setCaption] = useState(props?.posts?.caption as string);
+  const [image, setImage] = useState(props?.posts?.image);
+  const [imageUrl, setImageUrl] = useState("");
+  const storage = getStorage(app);
+  const navigate = useNavigate();
+  const uploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //@ts-ignore
+    setImageUrl(URL.createObjectURL(e?.target?.files[0]));
+    //@ts-ignore
+    setImage(e?.target?.files[0]);
+  };
+  const updatePost = async () => {
+    if (image === props?.posts?.image) {
+      //@ts-ignore
+      await setDoc(doc(db, "posts", props?.posts?.id), {
+        caption: caption,
+        image: image,
+        createdAt: serverTimestamp(),
+        userId: auth?.currentUser?.uid,
+        userName: auth?.currentUser?.displayName,
+        userPfp: auth?.currentUser?.photoURL,
+      })
+        .then(() => {
+          toast({
+            title: "Success",
+            description: "Post updated succesfully",
+            status: "success",
+            duration: 6900,
+            isClosable: true,
+          });
+          navigate("/");
+        })
+        .catch((err) => {
+          toast({
+            title: "Error",
+            description: err?.message,
+            status: "error",
+            duration: 6900,
+            isClosable: true,
+          });
+        });
+    } else {
+      //@ts-ignore
+      const storageRef = ref(storage, `/images/${image.name + Date.now()}`);
+      //@ts-ignore
+      const uploadTask = uploadBytesResumable(storageRef, image);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (err) => {
+          toast({
+            title: "Error",
+            description: err?.message,
+            status: "error",
+            duration: 6900,
+            isClosable: true,
+          });
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref)
+            .then(async (url) => {
+              //@ts-ignore
+              await setDoc(doc(db, "posts", props?.posts?.id), {
+                caption: caption,
+                image: url,
+                createdAt: serverTimestamp(),
+                userId: auth?.currentUser?.uid,
+                userName: auth?.currentUser?.displayName,
+                userPfp: auth?.currentUser?.photoURL,
+              })
+                .then(() => {
+                  toast({
+                    title: "Success",
+                    description: "Post updated succesfully",
+                    status: "success",
+                    duration: 6900,
+                    isClosable: true,
+                  });
+                  navigate("/");
+                })
+                .catch((err) => {
+                  toast({
+                    title: "Error",
+                    description: err?.message,
+                    status: "error",
+                    duration: 6900,
+                    isClosable: true,
+                  });
+                });
+            })
+            .catch((err) => {
+              setLoading(false);
+              toast({
+                title: "Error",
+                description: err?.message,
+                status: "error",
+                duration: 6900,
+                isClosable: true,
+              });
+            });
+        }
+      );
+    }
   };
   return (
     <Flex
@@ -114,7 +247,7 @@ const Post = (props: Props) => {
               />
             </MenuButton>
             <MenuList>
-              <MenuItem gap="0.5rem">
+              <MenuItem gap="0.5rem" onClick={onEditOpen}>
                 <BiEdit size={20} color="#90CDF4" />
                 <Heading as="h4" size="sm" color="#90CDF4">
                   Edit
@@ -127,6 +260,50 @@ const Post = (props: Props) => {
                 </Heading>
               </MenuItem>
             </MenuList>
+            {/* edit modal stuff */}
+            <Modal isOpen={isEditOpen} onClose={onEditClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Update Post</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <Flex width="100%" gap="1rem" flexDirection="column">
+                    <Input
+                      variant="filled"
+                      placeholder="Caption"
+                      value={caption}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setCaption(e?.target?.value);
+                      }}
+                    />
+                    <input
+                      type="file"
+                      accept="image/*"
+                      placeholder="Select post image"
+                      onChange={uploadImage}
+                    />
+                  </Flex>
+                  {imageUrl ? (
+                    <Image src={imageUrl} alt="" width="100%" />
+                  ) : (
+                    <Image src={image} alt="" width="100%" />
+                  )}
+                </ModalBody>
+                <ModalFooter>
+                  <Button mr={3} onClick={onEditClose}>
+                    Close
+                  </Button>
+                  {caption?.length >= 5 &&
+                  caption?.length <= 100 &&
+                  image !== "" ? (
+                    <Button onClick={updatePost}>Update</Button>
+                  ) : (
+                    <Button disabled>Update</Button>
+                  )}
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+            {/* delete alert dialog stuff here */}
             <AlertDialog
               isOpen={isOpen}
               //@ts-ignore
