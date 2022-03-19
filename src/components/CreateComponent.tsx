97e9ddae55a167f0ffdda46e7dd8ccbea7ea59e7
Painman <painman@gmail.com>
2022-03-19 19:23:29 +0530

    Post create stuff done

--- src/components/CreateComponent.tsx
+++ src/components/CreateComponent.tsx
@@ -8,16 +8,103 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { BsImageFill } from "react-icons/bs";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
+import {
+  collection,
+  addDoc,
+  getFirestore,
+  serverTimestamp,
+} from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 const CreateComponent = () => {
   const [caption, setCaption] = useState("");
   const [image, setImage] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const navigate = useNavigate();
   const uploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     //@ts-ignore
     setImageUrl(URL.createObjectURL(e?.target?.files[0]));
+    //@ts-ignore
+    setImage(e?.target?.files[0]);
   };
   const toast = useToast();
+  const storage = getStorage(app);
+  const db = getFirestore(app);
+  const [loading, setLoading] = useState(false);
+  const createPost = async () => {
+    setLoading(true);
+    //@ts-ignore
+    const storageRef = ref(storage, `/images/${image.name + Date.now()}`);
+    const auth = getAuth(app);
+    //@ts-ignore
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (err) => {
+        setLoading(false);
+        toast({
+          title: "Error",
+          description: err?.message,
+          status: "error",
+          duration: 6900,
+          isClosable: true,
+        });
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then(async (url) => {
+            await addDoc(collection(db, "posts"), {
+              caption: caption,
+              image: url,
+              createdAt: serverTimestamp(),
+              userId: auth?.currentUser?.uid,
+              userName: auth?.currentUser?.displayName,
+              userPfp: auth?.currentUser?.photoURL,
+            })
+              .then(() => {
+                toast({
+                  title: "Success",
+                  description: "Post created succesfully",
+                  status: "success",
+                  duration: 6900,
+                  isClosable: true,
+                });
+                setLoading(false);
+                navigate("/");
+              })
+              .catch((err) => {
+                setLoading(false);
+                toast({
+                  title: "Error",
+                  description: err?.message,
+                  status: "error",
+                  duration: 6900,
+                  isClosable: true,
+                });
+              });
+          })
+          .catch((err) => {
+            setLoading(false);
+            toast({
+              title: "Error",
+              description: err?.message,
+              status: "error",
+              duration: 6900,
+              isClosable: true,
+            });
+          });
+      }
+    );
+  };
   return (
     <Flex flexDirection="column" gap="2rem" width="100%" height="max-content">
       <Heading as="h4" size="md">
@@ -69,15 +156,9 @@ const CreateComponent = () => {
         <Button
           colorScheme="purple"
           marginBottom="1rem"
-          onClick={() => {
-            toast({
-              title: "Not implemented yet.",
-              description: "Painman working on it so plz be patient.",
-              status: "warning",
-              duration: 6900,
-              isClosable: true,
-            });
-          }}
+          onClick={createPost}
+          isLoading={loading}
+          loadingText="Creating"
         >
           Create post
         </Button>
